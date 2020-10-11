9098442e5de18a5f507e19a3118f4d38ecebbeef
Yegor <bravo13blog@gmail.com>
2020-10-11 19:48:20 +0300

    INit db done

--- app.pl
+++ app.pl
@@ -6,7 +6,26 @@ use Dancer2::Plugin::Cache::CHI;
 use LWP::UserAgent;
 use Syntax::Keyword::Try;
 
-
+# test handler to test if app is runned
 get '/' => sub {
 	status_ok({ok => 1});
 };
+
+
+# Just inits tables in DB
+sub _init_db_schema {
+	my @queries = (
+		'CREATE TABLE IF NOT EXISTS images (
+			id TEXT PRIMARY KEY,
+			author TEXT,
+			camera TEXT,
+			full_picture TEXT,
+			tags TEXT,
+			cropped_picture TEXT
+		)',
+	);
+
+	for my $query (@queries){
+		database()->prepare($query)->execute;
+	}
+}
