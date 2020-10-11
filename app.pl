de8f6f9f01378b265f4c5c8ba7ad85db6819aa20
Yegor <bravo13blog@gmail.com>
2020-10-11 20:05:12 +0300

    caching images

--- app.pl
+++ app.pl
@@ -29,3 +29,50 @@ sub _init_db_schema {
 		database()->prepare($query)->execute;
 	}
 }
+
+# To store image data DB is used. But to allow searching by meta
+# some faster storage required. That is fo memcache. So just whe image data
+# recieved it saved to DB. Meta tags parsed and stored to memcache. LInking
+# between DB entry and cached meta done by image id.
+sub _cache_image_data {
+	my $image = shift;
+
+    # collecting all meta tags in one list
+	my @metas = grep {$_} split(/\s?#/, $image->{tags} // ''), split(/\s/, $image->{camera} // ''), split(/\s/, $image->{author} // '');
+
+    # adding entry to cached lists
+	for my $tag (@metas){
+		my @entities = _unpack_cache_data(cache->get($tag) // '');
+		push @entities, $image->{id};
+		cache->set($tag, _pack_cache_data(@entities));
+	}
+
+	try {
+        # creating entry in db
+		database()->quick_insert( 'images', $image );
+	} catch {
+		die $@;
+	}
+}
+
+# unpack data after getting from memcache
+sub _unpack_cache_data {
+	my $string = shift;
+	my %table;
+	for my $item( split(',', $string) ){
+		$table{$item} = 1;
+	}
+	return keys %table;
+}
+
+# pack data before putting to memcache
+sub _pack_cache_data {
+	my @entities = @_;
+	my %table;
+	for my $item (@entities){
+		$table{$item} = 1;
+	};
+	return join(',', keys(%table));
+}
+
+start;
